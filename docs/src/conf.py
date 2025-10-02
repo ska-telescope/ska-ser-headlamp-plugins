4d3c1c06a3dfac4a827485e87d0bb82de0d0af40
Pedro Osório <pedroosorio.eeic@gmail.com>
2025-10-02 17:02:47 +0100

    ST-2466: Bump version

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -36,9 +36,9 @@ author = "SKA System Team"
 # built documents.
 #
 # The short X.Y.Z version.
-version = '0.0.2'
+version = '0.0.3'
 # The full version, including alpha/beta/rc pre-release tags.
-release = '0.0.2'
+release = '0.0.3'
 
 # -- General configuration ------------------------------------------------
 
